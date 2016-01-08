d609f5eb8e4ec2b7119fa7d5d504b25645faf4cc
Hani <hani@sei.ir>
2016-01-08 11:53:00 +0330

    Improve drawing sample to create smoother drawings

--- example/drawing/drawing.cpp
+++ example/drawing/drawing.cpp
@@ -7,11 +7,26 @@ int __stdcall wWinMain(HINSTANCE, HINSTANCE, LPWSTR, int)
 {
 	junior::window main(L"Junior Drawing");
 
+	int px = -1, py = -1;
+
 	while (auto e = junior::wait_for_event())
 	{
-		if (e == junior::event_type::mouse_move && e.mouse.is_button_pressed.left)
+		if (e == junior::event_type::mouse_lbutton_down)
 		{
-			e.window->draw_circle(e.mouse.x, e.mouse.y, 2);
+			px = e.mouse.x;
+			py = e.mouse.y;
+		}
+
+		if (e == junior::event_type::mouse_lbutton_up)
+		{
+			px = py = -1;
+		}
+
+		if (e == junior::event_type::mouse_move && e.mouse.is_button_pressed.left && px != -1)
+		{
+			e.window->draw_line(px, py, e.mouse.x, e.mouse.y);
+			px = e.mouse.x;
+			py = e.mouse.y;
 		}
 
 		if (e == junior::event_type::key_down && e.key.code == VK_ESCAPE)
